bb38dc82549c5946a849ef02b4427359a0b5938d
Rashid Korko <benooo.sy.96@gmail.com>
2025-05-14 18:25:09 +0330

    models-and-tables

---
+++ Modules/Category/app/Models/Brand.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Modules\Category\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Modules\Product\app\Models\Product;
+
+// use Modules\Category\Database\Factories\BrandFactory;
+
+class Brand extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     */
+    protected $fillable = ['name'];
+
+    public function products(): BelongsToMany
+    {
+        return $this->belongsToMany(Product::class);
+    }
+
+    // protected static function newFactory(): BrandFactory
+    // {
+    //     // return BrandFactory::new();
+    // }
+}
