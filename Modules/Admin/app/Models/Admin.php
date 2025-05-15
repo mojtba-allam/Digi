a0affdf84fd1ea59e54cc58b20dd8001b58b5976
Abdullah Ismail <abdullahismail612005@example.com>
2025-05-15 12:45:21 +0330

    relationships_for_models

--- Modules/Admin/app/Models/Admin.php
+++ Modules/Admin/app/Models/Admin.php
@@ -1,10 +1,14 @@
 <?php
 
-namespace Modules\Admin\Models;
+namespace Modules\Admin\app\Models;
 
 use Couchbase\Role;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Modules\Authorization\Models\TemporaryPermission;
+
 // use Modules\Admin\Database\Factories\AdminFactory;
 
 class Admin extends Model
@@ -17,13 +21,14 @@ class Admin extends Model
 
     protected $fillable = ['name', 'email', 'password', 'role_id'];
 
-    public function role()
+    public function role(): BelongsTo
     {
         return $this->belongsTo(Role::class);
     }
 
-    public function permissions()
+    public function temporary_permissions(): HasMany
     {
-        return $this->belongsToMany(Permission::class, 'temporary_permissions');
+        return $this->hasMany(TemporaryPermission::class);
     }
+
 }
--- Modules/Business/app/Models/Vendor.php
+++ Modules/Business/app/Models/Vendor.php
@@ -1,10 +1,17 @@
 <?php
 
-namespace Modules\Business\Models;
+namespace Modules\Business\app\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 use Modules\Authorization\Models\User;
+use Modules\CommissionAndPayout\Models\Commission;
+use Modules\CommissionAndPayout\Models\Payout;
+use Modules\CommissionAndPayout\Models\Settlement;
 
 // use Modules\Business\Database\Factories\VendorFactory;
 
@@ -18,18 +25,45 @@ class Vendor extends Model
         'status',
     ];
 
-    public function user()
+    public function user(): BelongsTo
     {
         return $this->belongsTo(User::class);
     }
 
-    public function profile()
+    public function products() : HasMany
+    {
+        return $this->hasMany(Product::class);
+    }
+
+    public function order(): BelongsToMany
+    {
+        return $this->belongsToMany(Order::class);
+    }
+
+
+    public function vendor_profile() : HasOne
     {
         return $this->hasOne(VendorProfile::class);
     }
 
-    public function commission()
+    public function commissions(): HasMany
     {
-        return $this->hasOne(VendorCommission::class);
+        return $this->hasMany(Commission::class);
+    }
+    public function vendor_commissions(): HasMany
+    {
+        return $this->hasMany(VendorCommission::class);
+    }
+    public function return_requests(): HasMany
+    {
+        return $this->hasMany(ReturnRequest::class);
+    }
+    public function settlements() : HasMany
+    {
+        return $this->hasMany(Settlement::class);
+    }
+    public function payouts(): HasMany
+    {
+        return $this->hasMany(Payout::class);
     }
 }
